0aaa230ada38296e6f4aaf584c8b648c6bb992c7
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-05-06 22:03:06 +0200

    Refactor screen exports and add new screens to the export in index.ts

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,6 @@
-export {default as LoginScreen} from "./LoginScreen";
-export { default as CodeScanningScreen } from "./CodeScanningScreen";
+export { default as LoginScreen } from "./Login/LoginScreen";
+export { default as QRCodeScanningScreen } from "./QRCodeScanning/CodeScanningScreen";
+export { default as ClockingSelectionScreen } from "./ClockingSelection/ClockingSelectionScreen";
+export { default as HomeScreen } from "./Home/HomeScreen";
+export { default as LeavesScreen } from "./Leaves/LeavesScreen";
+export { default as ClockingHistoryScreen } from "./ClockingHistory/ClockingHistoryScreen";
