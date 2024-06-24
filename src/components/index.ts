ee45cbaf1ca510cd21bdff09f1f7fd30e3ee77b4
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-06-24 03:38:54 +0200

    Added component function exports to their respective index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export { default as BurchLogo } from "./BurchLogo";
 export { default as LoginButton } from "./LoginButton";
 export { default as AccessCodeInput } from "./AccessCodeInput";
 export { default as LoginScreenCredits } from "./LoginScreenCredits";
+export { default as FloatingCard } from "./FloatingCard";
+export { default as ActivityCard } from "./HomeActivityCard";
+export { default as LocationCard } from "./HomeLocationCard";
