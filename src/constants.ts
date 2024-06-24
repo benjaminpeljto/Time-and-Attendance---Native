bc40280c0290767ef1fe783017e19411ee6c45e1
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-06-24 03:17:13 +0200

    Update BASE_URL_HOME and BASE_URL_DJULAGIN in constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+//export const BASE_URL_HOME = 'http://192.168.31.111'
+export const BASE_URL_HOME = 'http://192.168.31.111'
+export const BASE_URL_DJULAGIN = 'http://192.168.31.111'
+export const PORT = ":8000"
+export const BASE_SUB="/api/native"
