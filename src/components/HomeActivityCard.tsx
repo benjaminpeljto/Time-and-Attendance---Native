64565cae4b95e05f27096de5e88526d7b607b0e6
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-06-25 03:49:05 +0200

    Modified HomeActivityCard component. Added the logic to view all button and loading states

--- src/components/HomeActivityCard.tsx
+++ src/components/HomeActivityCard.tsx
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 import { HomeActivityCardProps } from "../utils/types";
 
 export default function HomeActivityCard({
   clockInTime,
   clockOutTime,
+  isLoading,
 }: HomeActivityCardProps) {
   const [currentDate, setCurrentDate] = useState("");
+  const [isPressed, setIsPressed] = useState(false);
 
   useEffect(() => {
     const now = new Date();
-
     const formattedDate = new Intl.DateTimeFormat("en-US", {
       month: "long",
       day: "2-digit",
       year: "numeric",
     }).format(now);
-
     setCurrentDate(formattedDate);
   }, []);
+
+  const handleViewAllPress = () => {
+    Alert.alert("View All", "Displaying all activities.");
+  };
 
   return (
     <>
@@ -33,13 +43,29 @@ export default function HomeActivityCard({
           />
           <Text style={styles.activityTitle}>Today's activity</Text>
         </View>
-        <Text style={styles.cardAction}>View All</Text>
+        <Pressable
+          style={({ pressed }) => [
+            styles.headerSub,
+            pressed && styles.pressedButton,
+          ]}
+          onPressIn={() => setIsPressed(true)}
+          onPressOut={() => setIsPressed(false)}
+          onPress={handleViewAllPress}
+        >
+          <Text style={[styles.cardAction, isPressed && styles.pressedText]}>
+            View All
+          </Text>
+        </Pressable>
       </View>
       <View style={styles.card}>
         {/* Clock In Section */}
         <View style={styles.row}>
           <View style={styles.iconWrapper}>
-            <AntDesign name='login' size={24} color='#013B6D' />
+            {isLoading ? (
+              <ActivityIndicator size={24} color='rgba(1, 59, 109, 1)' />
+            ) : (
+              <AntDesign name='login' size={24} color='#013B6D' />
+            )}
           </View>
           <View style={styles.info}>
             <Text style={styles.activityText}>Clock In</Text>
@@ -53,14 +79,19 @@ export default function HomeActivityCard({
         {/* Clock Out Section */}
         <View style={styles.row}>
           <View style={styles.iconWrapper}>
-            <AntDesign name='logout' size={24} color='#013B6D' />
+            {isLoading ? (
+              <ActivityIndicator size={24} color='rgba(1, 59, 109, 1)' />
+            ) : (
+              <AntDesign name='logout' size={24} color='#013B6D' />
+            )}
           </View>
+
           <View style={styles.info}>
             <Text style={styles.activityText}>Clock Out</Text>
             <Text style={styles.dateText}>{currentDate}</Text>
           </View>
           <Text style={styles.timeText}>
-            {clockOutTime ? clockOutTime.slice(0, 6) : "--:--"}
+            {clockOutTime ? clockOutTime.slice(0, 5) : "--:--"}
           </Text>
         </View>
       </View>
@@ -79,7 +110,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 3,
-    marginBottom: 30,
+    marginBottom: 25,
   },
   row: {
     flexDirection: "row",
@@ -114,7 +145,7 @@ const styles = StyleSheet.create({
   separator: {
     height: 1,
     backgroundColor: "#E6E6E6",
-    marginVertical: 5, // Reduced margin to minimize space between rows
+    marginVertical: 5,
   },
   activityHeader: {
     flexDirection: "row",
@@ -125,6 +156,8 @@ const styles = StyleSheet.create({
   headerSub: {
     flexDirection: "row",
     alignItems: "baseline",
+    padding: 5,
+    borderRadius: 5,
   },
   activityTitle: {
     fontSize: 15,
@@ -137,5 +170,11 @@ const styles = StyleSheet.create({
     color: "#0268C0",
     fontWeight: "bold",
     marginLeft: 5,
+  },
+  pressedButton: {
+    backgroundColor: "#CCE7FF",
+  },
+  pressedText: {
+    color: "#013B6D",
   },
 });
