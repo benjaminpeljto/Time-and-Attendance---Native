24eb57bb18adcd32835b22bff9a5e2fc8d97d435
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-06-28 00:25:01 +0200

    Modified constants.ts file. Configured it to use process.env for .env specific variables.

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,2 @@
-//export const BASE_URL_HOME = 'http://192.168.31.111'
-export const BASE_URL_HOME = 'http://192.168.31.111'
-export const BASE_URL_DJULAGIN = 'http://192.168.31.111'
-export const PORT = ":8000"
-export const BASE_SUB="/api/native"
+export const BASE_URL_HOME = process.env.EXPO_PUBLIC_SERVER_URL;
+export const BASE_SUB = process.env.EXPO_PUBLIC_SERVER_SUB_URL;
